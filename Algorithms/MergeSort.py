758884561c23d30b83495f37c97a8198418a43e0
Tree-Nulbo <exopas58@gmail.com>
2019-03-21 16:29:39 +0900

    Mergesort Algorithm
    Mergesort algorithm practice

---
+++ Algorithms/MergeSort.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 16:27:41 2019
+
+@author: cross
+"""
+
+class Method:
+    L_list = None
+    R_List = None
+    
+    def mergesort(self, n, S):
+        
+    def merge(self,)
