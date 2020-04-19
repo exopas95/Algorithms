1c5a74e7bf5e3fc61b060e29e5ab524919d02371
exopas95 <exopas58@gmail.com>
2020-04-19 23:51:33 +0900

    백준 #4673
    10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.

--- Baek_Joon/7_함수/1_정수_n개의_합.py
+++ Baek_Joon/7_함수/1_정수_n개의_합.py
@@ -9,9 +9,12 @@ a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a
 a에 포함되어 있는 정수 n개의 합 (정수)
 """
 import sys
+
+
 def solve(a):
     ans = sum(a)
     return ans
+
 
 List = list(map(int, sys.stdin.readline().split()))
 
